6a02144ba1d7d8abcbdae5ce6cf7b6e599cfde1d
Scott Harrington <scottharringtonsd@gmail.com>
2025-12-02 15:38:37 +0000

    add meal and drink sections

--- src/pages/RecipeSearchPage.tsx
+++ src/pages/RecipeSearchPage.tsx
@@ -8,15 +8,26 @@ interface RecipeSearchPageProps {
 const RecipeSearchPage = ({ recipes }: RecipeSearchPageProps) => {
   return (
     <>
-      <h1 className="text-7xl italic text-center">Search Recipes</h1>
+      <h1 className="text-8xl text-center p-3">Search Recipes</h1>
       <p className="text-3xl text-center">
         Search through a selection of salivating suppers and sumptuous snacks,
         guaranteed to satiate you and silence any sufferings of your stomach.{" "}
       </p>
+      <h2 className="text-5xl p-6">Meals</h2>
       <div className="p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto">
-        {recipes.map((recipe, index) => (
-          <RecipeCard key={index} recipe={recipe} />
-        ))}
+        {recipes
+          .filter((rec) => rec.type === "meal")
+          .map((recipe, index) => (
+            <RecipeCard key={index} recipe={recipe} />
+          ))}
+      </div>
+      <h2 className="text-5xl p-6">Drinks</h2>
+      <div className="p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto">
+        {recipes
+          .filter((rec) => rec.type === "drink")
+          .map((recipe, index) => (
+            <RecipeCard key={index} recipe={recipe} />
+          ))}
       </div>
     </>
   );
